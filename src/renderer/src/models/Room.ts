dfe356d4ccdd4773207991107d5c17c435d2d97c
es5es5 <es5e5s@nate.com>
2023-12-19 17:23:35 +0900

    RoomItem

--- src/renderer/src/models/Room.ts
+++ src/renderer/src/models/Room.ts
@@ -1,10 +1,24 @@
 import { User } from './User'
 
 export interface RoomItem {
+  createdAt: string
   id: string
   name: string
-  ownerId?: string
-  owner?: User
-  user?: string[]
-  // user?: Array<User>
+  ownerId: string
+  ownerName: string
+  ownerProfile: string
+}
+
+export interface RoomDetail {
+  room: RoomItem
+  players?: Array<User>
+}
+
+export interface MessageItem {
+  roomId: string
+  message: string
+  senderId: string
+  senderName: string
+  senderProfile: string
+  isMe?: boolean
 }
